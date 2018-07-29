729ee5194cf32cbad2e568f36f0f0c6f8fc65d51
Anders Christiansen Sørby <user8715@gmail.com>
2018-07-29 12:38:54 +0900

    Update version

---
+++ platforms/android/app/build/intermediates/assets/debug/www/assets/js/bluetooth.js
@@ -0,0 +1,269 @@
+var bth = (function($, window, db, loader) {
+
+    var _devices        = {};
+    var _hide           = false;
+    var _listDevices    = false;
+    var _retry          = 0;
+    var _maxRetry       = 20;
+    var _wasConnected   = false;
+
+    /**
+     * ON EVENTS (CALLBACK FUNCTIONS)
+     */
+    function _hideAll()
+    {
+        $(".status-on").hide();
+        $(".status-off").hide();
+        $(".no-devices").hide();
+        $(".connected").hide();
+        loader.close();
+    }
+
+
+    function _construct()
+    {
+        $(document).on("deviceready", function(){
+            _goButton();
+            _swipeArea();
+            _touchArea();
+            _checkEnabled();
+
+            var getDevices = db.get("devices");
+
+            _.each(getDevices, function(el, index, array){
+                console.log(el);
+                console.log(array[index]);
+            });
+
+            if (getDevices) {
+                _devices = getDevices;
+            }
+        });
+    }
+
+
+    function _checkEnabled()
+    {
+        _hideAll();
+        bluetoothSerial.isEnabled(
+            _onEnabled,
+            function() {
+                bluetoothSerial.enable(
+                    _onEnabled,
+                    _onDisabled
+                );
+            }
+        );
+    }
+
+
+    function _onEnabled()
+    {
+        console.log("Bluetooth Enabled");
+        $(".status-on").show();
+        $('body').addClass("on");
+        $('body').removeClass("off");
+        bluetoothSerial.isConnected(_onConnected, _onNotConnected);
+    }
+
+
+    function _onDisabled()
+    {
+        console.log("Bluetooth Disabled");
+        $(".status-off").show();
+        $('body').addClass("off");
+        $('body').removeClass("on");
+    }
+
+
+    function _onConnected(device)
+    {
+        console.log("CONNECTED, send commands");
+        _hideAll();
+        _setLastConnected();
+
+        $(".connected").show();
+        _retry = 0;
+    }
+
+
+    function _onNotConnected(device)
+    {
+        console.log("NOT CONNECTED, listing devices");
+
+        // I swear we were connected, just now!
+        if (_wasConnected) {
+            _checkReconnect("Connection lost, attempting to reconnect");
+        }
+
+        // Should we list the devices?
+        if ( ! _listDevices)
+        {
+            _checkReconnect("Attempting to connect to previous server");
+        }
+        bluetoothSerial.list(_onDeviceList);
+    }
+
+
+    function _onDeviceList(devices)
+    {
+        // No paired devices
+        if (devices.length === 0) {
+            _onNoDevices();
+
+        } else {
+
+            $('div.devices').show();
+            var deviceList = "";
+            _.each(devices, function(device) {
+                deviceList += '<li data-id="' + device.id + '" onClick="bth.connect(\'' + device.id + '\');">';
+                deviceList += device.name;
+                deviceList += '</li>';
+
+                _devices[device.id] = device.name;
+            });
+
+            db.set(
+                "devices",
+                devices
+            );
+
+            $('ul.devices').html(deviceList);
+        }
+    }
+
+
+    function _onNoDevices()
+    {
+        _hideAll();
+        $(".no-devices").show();
+    }
+
+
+    function _connect(device)
+    {
+        $('[data-id="' + device + '"]').addClass("active");
+        db.set("last_device_attempt", device);
+        console.log("Connecting to " + _devices[device] + "....");
+        loader.set("Connecting to " + _devices[device] + "....");
+
+        bluetoothSerial.connect(
+            device,
+            function (message){
+                console.log("CONNECTED SUCCESSFULLY");
+                loader.close();
+                console.log(message);
+                _onConnected();
+            },
+            function(){
+                console.log("FAILED TO CONNECT");
+                loader.error("Failed to connect to the server. Please ensure that it is running", true, 10000);
+            }
+        );
+    }
+
+
+    function _setLastConnected() {
+        if (db.get("last_device_attempt")) {
+            db.set("last_device_success",
+                db.get("last_device_attempt")
+            );
+        }
+    }
+
+
+    function _reconnect(device) {
+        if (_retry <= _maxRetry) {
+
+            _retry += 1;
+            loader.set("Attempting to reconnect (" + _retry + " of " + _maxRetry + ")");
+            _connect(device);
+
+        }  else {
+            loader.error("Failed to reconnect", true, 10000);
+        }
+    }
+
+
+    // Attempt to reconnect to the last item
+    function _checkReconnect(message) {
+        if (db.get("last_device_success")) {
+            _reconnect(db.get("last_device_success"));
+            loader.set(message);
+        }
+    }
+
+    // We have a swipe are we need to be able to detect
+    function _swipeArea() {
+        var actions         = {};
+        var actionText      = {};
+        actions["right"]    = "prev";
+        actions["left"]     = "next";
+        actions["up"]       = "play";
+        actions["down"]     = "stop";
+        actionText["right"] = "Previous";
+        actionText["left"]  = "Next";
+        actionText["up"]    = "Play/Pause";
+        actionText["down"]  = "Stop";
+
+        $(".swipearea").swipe({
+            swipe:function(event, direction, distance, duration, fingerCount){
+                if (actions[direction]) {
+                    var messageElement = $(this).children('.swipearea__message');
+                    messageElement.text(actionText[direction]);
+                    window.clearTimeout(window.messageTimeout);
+                    window.messageTimeout = window.setTimeout(function(){
+                        messageElement.text('Swipe Area');
+                    }, 1000);
+                    bth.action(actions[direction]);
+                }
+            },
+            threshold: $(".swipearea").hasClass('on') ? 80 : 0
+        });
+    }
+
+
+    // The skip left and right buttons also need listeners
+    function _touchArea() {
+        $(".touchable").on("click touchstart", function(){
+          console.log("Tap Detected: " + $(this).data("action"));
+          bth.action($(this).data("action"));
+        });
+        $(".action-button").on("click", function(){
+            console.log("Tap Detected: " + $(this).data("action"));
+            bth.action($(this).data("action"));
+        });
+    }
+
+
+    // Manage Manual Actions
+    function _action(value) {
+        bluetoothSerial.write(value, function (data) {
+            console.log(data);
+        }, function(data){
+            console.log(data);
+        });
+    }
+
+    // Go Button event listener
+    function _goButton() {
+        $("input[type='text']").on("keydown", function(event){
+            if (event.keyCode == 9 || event.keyCode == 13) {
+                _action( $(this).val() );
+            }
+        });
+    }
+
+
+    /**
+     * Auto-initilze and construct the module object
+     */
+    _construct();
+    return {
+        // Only the connect function is exposed at the moment
+        connect: _connect,
+        reconnect: _reconnect,
+        action: _action
+    };
+
+})(jQuery, window, db, loader);
---
+++ platforms/android/app/src/main/assets/www/assets/js/dragon.js
@@ -0,0 +1,219 @@
+var dragon = (function($){
+
+    // Reset the env
+    var _startX             = 0;
+    var _startY             = 0;
+    var _endX               = 0;
+    var _endY               = 0;
+    var _dragging           = false;
+    var _trottleInput       = 10;
+    var _pointers           = 0;
+    var _startTime          = Date.now();
+    var _previousTime       = _startTime;
+    var _lastDrag           = _startTime;
+    var _endTime            = Date.now();
+    var _slow               = false;
+    var _tapping            = false;
+    var _tapEventMax        = 3000;
+
+
+    // Get the coords regardless whether it's a mouse or touch event
+    var pointerPositionXY = function(e){
+        var out = {x:0, y:0};
+        var touchEvents = ["touchstart", "touchmove", "touchend", "touchcancel"];
+        var mouseEvents = ["mousedown", "mouseup", "mousemove", "mouseover", "mouseenter", "mouseleave", "mouseout"];
+
+        if (touchEvents.indexOf(e.type)){
+
+            // Touch Type events
+            var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+            _pointers = (e.originalEvent.touches.length || e.originalEvent.changedTouches.length);
+            //console.log(_pointers);
+            out.x = touch.pageX;
+            out.y = touch.pageY;
+
+        } else if (mouseEvents.indexOf(e.type)){
+            // Mouse Type events
+            out.x = e.pageX;
+            out.y = e.pageY;
+        }
+        return out;
+    };
+
+
+    // Create the handlers
+    $("[data-type='drag-area']").bind({
+        mousedown:      function(event) { _activate(event); },
+        touchstart:     function(event) { _activate(event); },
+        mouseup:        function(event) { _complete(event); },
+        mouseleave:     function(event) { _complete(event); },
+        touchend:       function(event) { _complete(event); },
+        touchleave:     function(event) { _complete(event); }
+    });
+
+
+
+/*    $("[data-type='drag-area']").on('tap', function() {
+        console.log('TAPPING');
+    });
+
+
+    $("[data-type='drag-area']").on('doubletap', function() {
+        console.log('DOUBLE TAPPING');
+    });*/
+
+
+    // Throttle the mouse moves somewhat, to ensure we do not flood the comms
+    $("[data-type='drag-area']").on('touchmove mousemove', _.throttle(
+        function(event){
+            _track(event);
+        },_trottleInput)
+    );
+
+    // We need to get starting coords, and set the dragging to enabled
+    function _activate(event) {
+        event.preventDefault();
+
+        _previousTime = _startTime;
+        _startTime    = Date.now();
+
+        var getXY   = pointerPositionXY(event);
+        //console.log(_pointers);
+        _startX     = getXY.x;
+        _startY     = getXY.y;
+    }
+
+    // On mouse move / touch move, this tracks the position and sends the commands
+    function _track(event) {
+        _dragging = true;
+        _lastDrag = Date.now();
+        event.preventDefault();
+
+        // Get the current position, and the distance we have since dragged
+        var getXY       = pointerPositionXY(event);
+        _endX           = getXY.x;
+        _endY           = getXY.y;
+		_getDistance    = _distance();
+
+        // The dragging logic, slow v.s. tapping v.s. dragging
+        _tapping = (_getDistance.x < 1.2 && _getDistance.x > -1.2 && _getDistance.y < 1.2 && _getDistance.y > -1.2);
+        _slow = (_getDistance.x < 3 && _getDistance.x > -3 && _getDistance.y < 3 && _getDistance.y > -3);
+        dragged = (!_tapping); // allow some more space before dragging
+
+        //console.log("dragged:", dragged);
+        // console.log(
+        //     "SLOW:", _slow,
+        //     "x:", _getDistance.x,
+        //     "y:", _getDistance.y,
+        //     "DB:", db.get('mouseSensitivity')
+        // );
+
+        if (dragged && !_getDistance.x && !_getDistance.y) {
+            _lastDrag = Date.now();
+        }
+
+        if (!_getDistance.x && !_getDistance.y) {
+            //console.debug("EMPTY COORDS!");
+            _tapping = true;
+            _dragging = false;
+        }
+
+        // Send the command via the BTH
+        bth.action(
+            JSON.stringify({
+                action: "mouse-move",
+                x: (_slow) ? _getDistance.x : ( _getDistance.x * Number(db.get('mouseSensitivity')) ),
+                y: (_slow) ? _getDistance.y : ( _getDistance.y * Number(db.get('mouseSensitivity')) )
+            })
+        );
+
+        // Set the start to the current positions. This ensures that movements are relative
+        _startX = _endX;
+        _startY = _endY;
+    }
+
+    // Once complete, just set the dragging to disabled
+    function _complete(event){
+        event.preventDefault();
+
+        _handleClicks();
+
+        if (_dragging) {
+            _dragging = false;
+        }
+    }
+
+
+    // As the name indicates this is to check if a click has occurred.
+    // Single quick tap for click
+    // Longer Single Tap for right click
+    // Multiple sequential clicks
+    function _handleClicks(){
+
+        /*console.log(
+            "SLOW:",
+            _slow,
+            "TAPPING:",
+            _tapping
+        );*/
+
+        _endTime            = Date.now();
+        var timeSpent       = (_endTime - _startTime);
+        var timeSpentBefore = (_endTime - _previousTime);
+        var lastDragged = (Date.now() - _lastDrag);
+
+        // Definitely not clicking when dragging!
+        if (!_tapping || timeSpent > (Number(db.get('rightClickSensitivity')) * 6)) {
+            return false;
+        }
+
+        // console.log(
+        //     "NOW: " + timeSpent,
+        //     "BEFORE: " + timeSpentBefore,
+        //     "CLICK THRESHOLD: ", Number(db.get('clickSensitivity')),
+        //     "RCLICK THRESHOLD: ", Number(db.get('rightClickSensitivity')),
+        //     "SLOW: ", _slow,
+        //     "DRAGGING: ", _dragging,
+        //     "TIME SPENT: ", timeSpent
+        // );
+
+        //console.log("lastDragged:", lastDragged);
+
+        // Right click
+        if (timeSpent > Number(db.get('rightClickSensitivity')) && lastDragged > 399) {
+            bth.action('rclick');
+            console.log("RIGHT CLICK!");
+            return true;
+        }
+
+        // Right click
+        if (timeSpentBefore <= (Number(db.get('clickSensitivity')) * 2) ) {
+            bth.action('click');
+            bth.action('click');
+            console.log("DOUBLE CLICK!");
+            return true;
+        }
+
+        // Default, just a click
+        if (timeSpent <= Number(db.get('clickSensitivity'))) {
+            bth.action('click');
+            console.log("SINGLE CLICK");
+            return true;
+        }
+    }
+
+
+    // We need to be able to determine how far the dragging action was
+    function _distance() {
+        var out = { x:0, y:0 };
+        if (_dragging) {
+            out.x = _endX - _startX;
+            out.y = _endY - _startY;
+        }
+        return out;
+    }
+
+    // Return Nothing
+    return {};
+
+})(jQuery);
